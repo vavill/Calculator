cfb16f3aab9cc989a0e39da69e65fb7aaf35f162
vavill <vavilov.alexey@mail.ru>
2024-02-27 20:39:50 +0300

    перенес всю логику в MainActivity заменил gridlayout на linearlayout

---
+++ app/src/main/java/com/example/calculator/MainActivity2.kt
@@ -0,0 +1,26 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.calculator.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val text = intent.getStringExtra(KEY)
+
+        binding.tv.text = text
+    }
+
+    companion object {
+        const val KEY = "key"
+    }
+}
